f9d9c65885a8547d911258d56d23db4197256b4d
vinicius bispo <vini.bispo125@gmail.com>
2023-02-27 18:37:37 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './App.css';
 import alface from "./imagens/alface.png"
 import laranja from "./imagens/laranja.png"
@@ -8,6 +8,9 @@ import manga from "./imagens/manga.png"
 import limao from "./imagens/limao.png"
 import beterraba from "./imagens/beterraba.png"
 import tomate from "./imagens/tomate.png"
+import carrinho from "./imagens/ShoppingCart.png"
+import soma from "./imagens/PlusCircle.png"
+import subtração from "./imagens/MinusCircle.png"
 class App extends Component {
   state = {
     horta: [
@@ -27,35 +30,46 @@ class App extends Component {
         img: `${manga}`
       },
       {
-        img: `${limao}`        
+        img: `${limao}`
       },
       {
         img: `${beterraba}`
       },
       {
-        img: `${tomate}`       
+        img: `${tomate}`
       }
     ]
   };
 
- render() {
-  return (
-    <div className='app'>
-    <div className="header">
-      <h1>HORTIFRUTI</h1> 
-      <p>VnW</p>
-      <h2>Nossos Produtos</h2>
-    </div>
+  render() {
+    return (
+      <div className='app'>
+        <div className="header">
+          <h1>HORTIFRUTI</h1>
+          <p>VnW</p>
+          <h2>Nossos Produtos</h2>
+        </div>
 
- <div className="card-container">
-{this.state.horta.map((item) => (
-  <div className="card">   
-    <img className="imgs" src={item.img} />
-  </div>
-))}
-</div>
-</div>
-  );
- }
+        <div className="card-container">
+          {this.state.horta.map((item) => (
+            <div className="card">
+              <img className="imgs" src={item.img} />
+            </div>
+          ))}
+        </div>
+        <div className='carrinho'>
+          <div className='secondCarrinho'>
+            <img className='carrinhoImg' src={carrinho} alt="carrinho de compras" />
+            <h3>Arraste o seu produto aqui para colocar no carriho</h3>
+
+          </div>
+          <div className='maisEmenos'>
+          <img className='sinal' src={soma} alt="sinal de soma" />
+          <img className='sinal' src={subtração} alt="sinal de subtração" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
